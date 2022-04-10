6212c3915992db70e68f86da3785e07135ed28ad
Ahyuan233 <46197311+Ahyuan233@users.noreply.github.com>
2022-04-10 10:40:07 -0400

    Add files via upload

---
+++ propeller_project/Piezo_test.c
@@ -0,0 +1,7 @@
+
+#include "simpletools.h"                      
+
+int main()                                    
+{
+   freqout(1,1000,3000);                      //freqout(Pin,Duration,Freq)  
+}
