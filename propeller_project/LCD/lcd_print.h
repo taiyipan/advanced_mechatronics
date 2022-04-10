747fcc90ad6748da9e219005063a9192037032f5
Ahyuan233 <46197311+Ahyuan233@users.noreply.github.com>
2022-04-10 02:05:20 -0400

    Add files via upload

---
+++ propeller_project/LCD/lcd_print.h
@@ -0,0 +1,17 @@
+
+#ifndef LCD_PRINT_H
+#define LCD_PRINT_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "simpletools.h"
+
+void lcd_print(char lcd_msg[]);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
