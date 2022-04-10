634113900f62a1b675feb8ce46cfdc4d7f184891
Ahyuan233 <46197311+Ahyuan233@users.noreply.github.com>
2022-04-10 02:05:41 -0400

    Add files via upload

---
+++ propeller_project/ultrasonic/ping_hcsr04.h
@@ -0,0 +1,19 @@
+
+#ifndef PING_HCSR04_H
+#define PING_HCSR04_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include "simpletools.h"
+
+int ping_hcsr04(int trig, int echo);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
+
+
