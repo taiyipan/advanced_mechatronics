e58402a0b0132866367a7bd16450cf201e3b5590
Ahyuan233 <46197311+Ahyuan233@users.noreply.github.com>
2022-04-10 10:27:25 -0400

    Add files via upload

---
+++ propeller_project/ultrasonic/Ultrasonic_test.c
@@ -0,0 +1,29 @@
+/*
+Pin 7 Right Ultrasonic
+Pin 8 trigger pin Front Ultrasonic
+Pin 9 echo pin Front Ultrasonic
+Pin 10 Left Ultrasonic 
+*/
+
+#include "ping.h"
+#include "ping_hcsr04.h"                      // non-parallax ultrasonic
+#include "simpletools.h"                      // Include simple tools
+
+
+const int right_ultrasonic_pin = 7;
+const int trigPin = 8;
+const int echoPin = 9;
+const int left_ultrasonic_pin = 10;
+
+int main()                                    // Main function
+{
+  while(1)
+  {
+   int cmDist = ping_cm(left_ultrasonic_pin);
+   printf("cmDist = %d\n",cmDist);
+   int front_dist = ping_hcsr04(trigPin, echoPin);
+   printf("front_dist = %d\n",front_dist);
+   pause(200);
+   
+  }  
+}
